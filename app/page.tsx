9be8466e585467a78b45b0257266482fd819fb69
Leo van der Zee <leo@MacBook-Pro-van-Leo-3.local>
2026-04-15 14:22:23 +0200

    feat: homepage met alle secties

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,27 @@
-export default function Home() {
+import type { Metadata } from 'next'
+import Hero from '@/components/home/Hero'
+import UspBar from '@/components/home/UspBar'
+import UnitsGrid from '@/components/home/UnitsGrid'
+import VergaderruimteTeaser from '@/components/home/VergaderruimteTeaser'
+import OverDeBasis from '@/components/home/OverDeBasis'
+import HuurderLogos from '@/components/home/HuurderLogos'
+import ContactCta from '@/components/home/ContactCta'
+
+export const metadata: Metadata = {
+  title: 'Kantoorruimte huren Leeuwarden | Bedrijfsverzamelgebouw de Basis',
+  description: 'Kantoorruimte huren in Leeuwarden. All-inclusive, flexibel maandelijks contract, gratis parkeren & WiFi. Vergader- en virtuele kantoren beschikbaar.',
+}
+
+export default function HomePage() {
   return (
-    <main>
-      <h1>De Basis</h1>
-    </main>
-  );
+    <>
+      <Hero />
+      <UspBar />
+      <UnitsGrid />
+      <VergaderruimteTeaser />
+      <OverDeBasis />
+      <HuurderLogos />
+      <ContactCta />
+    </>
+  )
 }
